40b2af616adcefe4ac12c8cbae707992eaa856b6
banana. <guimaraes26milena@gmail.com>
2024-11-19 16:39:27 -0300

    Aula3 e ex3(aula2) - javascript

--- Javascript/aula2 - condicionais/ex3/scripts/script.js
+++ Javascript/aula2 - condicionais/ex3/scripts/script.js
@@ -1,6 +1,6 @@
 function operacao(){
-    prompt("Escolha um número entre 1 e 7")
-    escolha = ('1) Domingo, 2) Segunda-feira, 3) Terça-Feira, 4) Quarta-Feira, 5) Quinta-Feira, 6) Sexta-Feira, 7) Sábado')
+    escolha =  prompt("Escolha um número entre 1 e 7")
+    // escolha = ('1) Domingo, 2) Segunda-feira, 3) Terça-Feira, 4) Quarta-Feira, 5) Quinta-Feira, 6) Sexta-Feira, 7) Sábado')
 
     switch(escolha){
         case '1':
---
+++ Javascript/aula3/script/script.js
@@ -0,0 +1,7 @@
+// DOM - FAZ A INTEGRAÇÃO DO HTML E DO JAVASCRIPT
+// URL - LOCATION
+// "BOTÃO DE VOLTAR" -HISTORY
+// "SITE/CONTEUDO" - DOCUMENTO
+
+//window.location.reload() -> reload serve p/ reiniciar a pagina infinitamente
+window.location = "https://www.google.com"

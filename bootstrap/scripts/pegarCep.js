78a767592c661836845f5eea20d74023770a4dc8
Milenaaguimaraess <guimaraes26milena@gmail.com>
2024-12-06 17:43:33 -0300

    bootstrap

---
+++ bootstrap/scripts/pegarCep.js
@@ -0,0 +1,14 @@
+document.querySelector('#cep').addEventListener('focusout',pegarCep)
+
+function pegarCep(){
+   cep = document.querySelector('#cep').value
+  
+   // buscar - fecth
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then((info) => {
+        return info.json()
+    }).then((dados) => {
+        console.log(dados)
+        document.getElementById('estado').value = dados.estado
+    
+})
+}
---
+++ bootstrap/scripts/script.js
@@ -0,0 +1,35 @@
+lista_categorias = []
+
+class Categoria {
+    constructor(titulo, img, desc, marca) {
+        this.titulo = titulo
+        this.img = img
+        this.desc = desc
+        this.marca = marca
+    }
+}
+
+lista_categorias.push(new Categoria('ARVORES DE NATAL', 'images/card1.jpg', 'AS MAIS BONITAS ARVORES DE NATAL PARA SUAS CELEBRAÇÕES DE FIM DE ANO.', 'Arvores de natal originais'))
+
+lista_categorias.push(new Categoria('Enfeites de arvores', 'images/card2.jpg', 'Confira nossa coleção de enfeites para arvores de natal exclusivas e personalizados.', 'enfeites de natal originais'))
+
+lista_categorias.push(new Categoria('Enfeites gerais', 'images/card3.jpg', 'Confira nossa coleção de enfeites gerais de natal exclusivas e personalizados.', 'enfeites de natal originais'))
+
+lista_categorias.forEach(categoria => {
+    document.querySelector('#categoriaItems').innerHTML += `
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${categoria.img}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${categoria.titulo}</h5>
+                    <p class="card-text">${categoria.desc}</p>
+                    <p class="card-text"><small class="text-body-secondary">${categoria.marca}</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+});

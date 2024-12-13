1b8ee41008b029e46deb81aaa2195fff89389aff
Felipe soares siqueira <guimaraes26milena@gmail.com>
2024-12-13 14:38:13 -0300

    atividade aula 7 - javascript

--- Javascript/aula7 - Promises/atividade/script/doceEsal.js
+++ Javascript/aula7 - Promises/atividade/script/doceEsal.js
@@ -1,51 +1,66 @@
+// const lista = {
+//     'doce' : [
+//          {
+//              'nome' : 'Ilha do medo',
+//              'Ingredientes' : '1 caixa de leite condensado; 1 caixa de leite condensado; 1 colher (sopa) de margarina sem sal e chocolate granulado',
+//              'Modo de Preparo' : 'Lorem'
+//          },
+//          {
+//              'nome' : 'Bruxa de blair',
+//              'Desc' : 'lorem'
+//          },
+//      ],
+//      'salgado' : [
+//          {
+//              'nome' : 'Roblox',
+//              'Desc' : 'lorem'
+//          },
+//          {
+//              'nome' : '',
+//              'Receita' : ''
+//          }    
+//      ],
+//      'Suco' : [
+//         {
+//             'nome' : 'Roblox',
+//             'Desc' : 'lorem'
+//         },
+//         {
+//             'nome' : '',
+//             'Receita' : ''
+//         }    
+//     ]
+//  }
+ 
+//  const resultadoo = lista.doce.map((elemento) => {
+//      document.querySelector('body').innerHTML += `
+//          <h3> ${elemento.nome}</h3>
+//      `
+//  })
+
+//  const resultado = lista.salgado.map((elemento) => {
+//     document.querySelector('body').innerHTML += `
+//         <h3> ${elemento.nome}</h3>
+//     `
+// })
+
+// const resultad = lista.salgado.map((elemento) => {
+//     document.querySelector('body').innerHTML += `
+//         <h3> ${elemento.nome}</h3>
+//     `
+// })
+
 const lista = {
-    'doce' : [
+    'filme' : [
          {
              'nome' : 'Ilha do medo',
-             'Ingredientes' : '1 caixa de leite condensado; 1 caixa de leite condensado; 1 colher (sopa) de margarina sem sal e chocolate granulado',
-             'Modo de Preparo' : 'Lorem'
+             'Desc' : 'Lorem'
          },
-         {
-             'nome' : 'Bruxa de blair',
-             'Desc' : 'lorem'
-         },
-     ],
-     'salgado' : [
-         {
-             'nome' : 'Roblox',
-             'Desc' : 'lorem'
-         },
-         {
-             'nome' : '',
-             'Receita' : ''
-         }    
-     ],
-     'Suco' : [
-        {
-            'nome' : 'Roblox',
-            'Desc' : 'lorem'
-        },
-        {
-            'nome' : '',
-            'Receita' : ''
-        }    
-    ]
- }
- 
- const resultadoo = lista.doce.map((elemento) => {
+     ]
+    }
+
+     const resultado = lista.filme.map((elemento) => {
      document.querySelector('body').innerHTML += `
          <h3> ${elemento.nome}</h3>
      `
  })
-
- const resultado = lista.salgado.map((elemento) => {
-    document.querySelector('body').innerHTML += `
-        <h3> ${elemento.nome}</h3>
-    `
-})
-
-const resultad = lista.salgado.map((elemento) => {
-    document.querySelector('body').innerHTML += `
-        <h3> ${elemento.nome}</h3>
-    `
-})
--- Javascript/aula7 - Promises/scripts/objectEntries.js
+++ Javascript/aula7 - Promises/scripts/objectEntries.js
@@ -37,7 +37,7 @@ const lista = {
         {
             'nome' : 'The last of us',
             'Desc' : 'lorem'
-        }    
+        },    
     ]
 }
 

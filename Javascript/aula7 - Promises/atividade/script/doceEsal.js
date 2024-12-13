5982ba802090bb21c9df6bc99ab9283dc3ae61a6
Felipe soares siqueira <guimaraes26milena@gmail.com>
2024-12-13 17:47:23 -0300

    atividade - bootstrap

--- Javascript/aula7 - Promises/atividade/script/doceEsal.js
+++ Javascript/aula7 - Promises/atividade/script/doceEsal.js
@@ -1,65 +1,14 @@
-// const lista = {
-//     'doce' : [
-//          {
-//              'nome' : 'Ilha do medo',
-//              'Ingredientes' : '1 caixa de leite condensado; 1 caixa de leite condensado; 1 colher (sopa) de margarina sem sal e chocolate granulado',
-//              'Modo de Preparo' : 'Lorem'
-//          },
-//          {
-//              'nome' : 'Bruxa de blair',
-//              'Desc' : 'lorem'
-//          },
-//      ],
-//      'salgado' : [
-//          {
-//              'nome' : 'Roblox',
-//              'Desc' : 'lorem'
-//          },
-//          {
-//              'nome' : '',
-//              'Receita' : ''
-//          }    
-//      ],
-//      'Suco' : [
-//         {
-//             'nome' : 'Roblox',
-//             'Desc' : 'lorem'
-//         },
-//         {
-//             'nome' : '',
-//             'Receita' : ''
-//         }    
-//     ]
-//  }
- 
-//  const resultadoo = lista.doce.map((elemento) => {
-//      document.querySelector('body').innerHTML += `
-//          <h3> ${elemento.nome}</h3>
-//      `
-//  })
-
-//  const resultado = lista.salgado.map((elemento) => {
-//     document.querySelector('body').innerHTML += `
-//         <h3> ${elemento.nome}</h3>
-//     `
-// })
-
-// const resultad = lista.salgado.map((elemento) => {
-//     document.querySelector('body').innerHTML += `
-//         <h3> ${elemento.nome}</h3>
-//     `
-// })
 
 const lista = {
-    'filme' : [
+    'doce' : [
          {
-             'nome' : 'Ilha do medo',
-             'Desc' : 'Lorem'
+             'Receita' : 'Brigadeiro',
+             'Ingredientes' : '1 caixa de leite condensado; 7 colheres (sopa) de achocolatado ou 4 colheres (sopa) de chocolate em pó; 1 colher (sopa) de margarina sem sal e chocolate granulado.'
          },
      ]
     }
 
-     const resultado = lista.filme.map((elemento) => {
+     const resultado = lista.doce.map((elemento) => {
      document.querySelector('body').innerHTML += `
          <h3> ${elemento.nome}</h3>
      `

d9692f2ea62bc499e6cbf89a0b47199f65014e64
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-02-28 17:26:03 -0300

    atualizando atividade - php

--- PHP/introducao/atividade_livro/script/script.js
+++ PHP/introducao/atividade_livro/script/script.js
@@ -9,7 +9,7 @@ function testes(){
     
     function carregarCatalogo(){
         var livros = [
-            ["images/livros/acotar.jpg", 'Corte de Nevoa e Fúria'],
+            ["images/livros/acotar.jpg", 'Corte de Nevoa e Fúria', 'Sarah J. Maas','À medida que Feyre é atraída para a sombria teia política e passional de Rhysand, uma guerra iminente aproxima-se e um mal muito mais perigoso do que qualquer rainha ameaça destruir tudo o que Feyre alguma vez tentou proteger.'],
            
         ]
        
@@ -18,6 +18,8 @@ function testes(){
         <div class="livro">
             <img src="${cadaLivro[0]}" alt="">
                 <h4>${cadaLivro[1]}</h4>
+                <h5>${cadaLivro[2]}</h5>
+                <p>${cadaLivro[3]}</p
                     
             <button>
                 Resumo

00130767bdda6e4310c988529b94a1344b6a2fd0
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-03-11 17:40:56 -0300

    pag de cadastro

---
+++ PHP/JOGOSPHP/assets/pages/cadastrar.php
@@ -0,0 +1,15 @@
+<?php
+    include "../components/baseHtml.php";
+?>
+
+<main>
+    <form action="../scripts/cadastro.php" method="post">
+    <input type="text" id="nome" name="nome">
+    <input type="email" id="email" name="email">
+    <input type="password" id="senha" name="senha">
+    <button type="submit">Cadastro</button>
+    </form>
+</main>
+<?php
+    include "../components/baseScriptsHtml.php";
+?>
--- PHP/JOGOSPHP/index.php
+++ PHP/JOGOSPHP/index.php
@@ -6,7 +6,27 @@
     <title>Login</title>
 </head>
 <body>
-    <a href="assets/pages/home.php">Home</a>
+    <!-- <a href="assets/pages/home.php">Home</a> -->
+
+    <main>
+        <form action="assets/scripts/login.php" method="post">
+        <!-- email -->
+         <div class="form-group">
+            <label for="email">Email: </label>
+            <input placeholder="Digite um Email"
+             type="email" require name="email" id="email">  
+         </div>
+         <!-- senha -->
+         <div class="form-group">
+            <label for="senha">Senha: </label>
+            <input placeholder="Digite uma senha"
+             type="password" require name="senha" id="senha">  
+         </div>
+         <button type="submit">LOGIN</button>
+         <p>Esqueceu a senha? <a href="#">Clique aqui</a></p>
+         <p>Não possui conta? <a href="assets/pages/cadastrar.php">Cadastre-se</a></p>
+        </form>
+    </main>
     
 </body>
 </html>

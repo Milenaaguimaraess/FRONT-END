5d44796e1146ca111d86b9a00bfcdb42b87786a8
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-03-24 17:37:52 -0300

    Integração DB

---
+++ PHP/carro/index.php
@@ -0,0 +1,48 @@
+<?php
+    include('db/conexao.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concenssionária</title>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <!-- ? é pra saber que é uma requisição -->
+            <a href="index.php?menu=home">HOME</a>
+            <a href="index.php?menu=lista">LISTA</a>
+        </nav>
+    </header>
+
+    <main>
+        <?php
+        // caso a pag seja home, eu chamo o arquivo do home, se for lista, chamo o arqv lista, se for bug, n chamo nenhum 
+        if(isset($_GET['menu'])){
+            $pagina = $_GET['menu'];
+        }else{
+            $pagina = "home"; 
+        }
+
+        switch($pagina){
+            case 'home':
+                include("pages/home/home.php");
+                break;
+            case 'lista':
+                include("pages/listaCarros/listaCarros.php");
+                break;
+            default:
+                include("pages/home/home.php");
+                break; 
+        }
+        ?>
+    </main>
+
+
+</body>
+</html>
---
+++ PHP/carro/pages/listaCarros/listaCarros.php
@@ -0,0 +1,33 @@
+<h1>VOCÊ ESTÁ NA LISTA DE CARROS!</h1>
+
+<table>
+    <tr>
+        <th>Modelo</th>
+        <th>Marca</th>
+        <th>Valor</th>
+        <th>Ano</th>
+        <th>Cor</th>
+    </tr>
+
+<?php
+    //UTILIZANDO BANCO DE DADOS
+    $sql = "SELECT * FROM carros";
+    // pedidos ao db (conexão com o db) -> query
+    $query = mysqli_query($conexao,$sql) or die("Erro na requisição!".mysqli_error($conexao));
+
+    // fetch_assoc -> vai acessar um query e contar os resultados
+    while($dados = mysqli_fetch_assoc($query)){
+        ?>
+        <tr>
+            <td><?=$dados['modeloCarro'] ?></td>
+            <td><?=$dados['marcaCarro'] ?></td>
+            <td><?=$dados['valorCarro'] ?></td>
+            <td><?=$dados['anoCarro'] ?></td>
+            <td><?=$dados['corCarro'] ?></td>
+        </tr>
+        <?php
+    }
+    ?>
+
+
+</table>

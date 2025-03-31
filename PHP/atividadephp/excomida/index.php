eaaf801670e4c847406712dc35df0a539c85974b
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-03-31 16:18:03 -0300

    atividade com db

--- PHP/atividadephp/excomida/index.php
+++ PHP/atividadephp/excomida/index.php
@@ -1,24 +1,42 @@
+<?php
+    include("db/conexao.php");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizzaria</title>
+    <title>Restaurante</title>
+    <link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
-    
 </body>
 </html>
- <?php
-    // CONSTANTES QUE SALVAM A CONFIGURAÇÃO DE CONEXÃO COM O BANCO
-    // DEFINE("SERVIDOR","localhost");
-    // DEFINE("USUARIO","root");
-    // DEFINE("SENHA","");
-    // DEFINE("BANCO","comida");
+    <header>
+        <h1>Cardápio</h1>
+    </header>
 
-    
-    $servername = "localhost";
-    $username = "root"; // Substitua pelo seu nome de usuário do MySQL
-    $password = "";     // Substitua pela sua senha do MySQL
-    $dbname = "comida";
-?>
+    <div id="comida">
+        <div class="lista">
+             <?php
+            $sql = "SELECT * FROM catalogo";
+            $query = mysqli_query($conexao, $sql) or die ("Erro na Requisição!".mysqli_error($conexao));
+            
+            while($dados = mysqli_fetch_assoc($query)){
+               ?>
+
+                    <ul>
+                        <div class="card">
+                            <li><?=$dados['nome']?></li>
+                            <li> R$<?=$dados['valor']?></li>
+                            <img src="<?=$dados['imagem']?>"></img>
+                        </div>
+                    </ul>
+                <?php
+            
+           }  
+        ?>
+        
+</div>
+    </div>

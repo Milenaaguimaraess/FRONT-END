b6a00a4e5273861673eb79c0f8cab0c6c5b19532
Milenaaguimaraess <guimaraes26milena@gmail.com>
2025-04-28 17:38:57 -0300

    correção do projeto receitas
    html, bootstrap e css

---
+++ PHP/Projetos/receitasCorrecao/pages/erro.php
@@ -0,0 +1,6 @@
+<main id=erroMain class="vh-100 container-fluid d-flex flex-column justify-content-center align-items-center">
+    <h1> Mermão, deu ruuuim</h1>
+    <a href="index.php?pagina=home">
+    <button class="btn btn-primary" type="button">Home</button>
+    </a>
+</main>
